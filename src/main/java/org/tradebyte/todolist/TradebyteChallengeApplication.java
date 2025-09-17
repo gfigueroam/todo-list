9e218a30864b215d3f8c5f296300b2b4a60401fc
Gloria Figueroa <gfigueroam05@gmail.com>
2025-09-17 13:08:49 +0200

    Initial spring-boot project

---
+++ src/main/java/org/tradebyte/todolist/TradebyteChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.tradebyte.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradebyteChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradebyteChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tradebyte/todolist/TradebyteChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradebyte.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebyteChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
